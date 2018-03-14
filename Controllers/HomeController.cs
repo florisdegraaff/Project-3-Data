8373a5e6340c174224917ea8d1d2aceeedc814cd
Floris de Graaff <florisdegraaff@hotmail.com>
2018-03-14 12:24:35 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using code.Models;
+
+namespace code.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Income()
+        {
+            ViewData["Title"] = "Income";
+
+            return View();
+        }
+
+        public IActionResult Hours()
+        {
+            ViewData["Title"] = "Hours";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
