68b37b3ab5fd6bb14021d788ce830728fc1971e8
Floris de Graaff <florisdegraaff@hotmail.com>
2018-03-21 09:01:35 +0100

    Created database connection with sample database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using code.Models;
+using Npgsql;
 
 namespace code.Controllers
 {
@@ -24,7 +25,34 @@ namespace code.Controllers
 
         public IActionResult Hours()
         {
+            
+            Dictionary<string, int> teachers = new Dictionary<string, int>();
+
             ViewData["Title"] = "Hours";
+
+            var connString = "Host=localhost;Username=postgres;Password=usE5k6mBMC1ciwv;Database=school";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT * FROM teacher", conn))
+                using (var reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                       teachers.Add(reader.GetString(0), reader.GetInt32(3));
+                    }
+                }
+            }
+
+            ViewData["Teachers"] = teachers;
+
+            //Console.WriteLine(ViewData["Teachers"]);
+
+            /*foreach (var teacher in ViewData["Teachers"])
+            {
+                Console.WriteLine(teacher)
+            }*/
 
             return View();
         }
